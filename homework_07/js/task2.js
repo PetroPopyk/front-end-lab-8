740b1b0664934397236607e112a0ca357fb596d0
Petro Popyk <happyfairy228@gmail.com>
2018-02-10 12:48:17 +0200

    final commit

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -4,17 +4,17 @@ number = Math.floor(Math.random() * step);
 var prize = 0;
 var max_prize = 10;
 var normal_prize = 5;
-var min_prize = 3;
+var min_prize = 2;
 var count = 0;
 var guess = 0;
 var status = true;
 if (start) {
     while (status) {
-        guess = prompt('Enter a number from 0 to ' + step + '\n' + 'Attempts left: ' + (3 - count) + '\n' + 'Total prize: ' + prize + '\n' + 'Possible prize on current attempt: ' + (count < 1 ? max_prize : (count < 2) ? normal_prize : min_prize) + '\n' + 'Enter guessed number: ');
+        guess = prompt('Enter a number from 0 to ' + step + '\n' + 'Attempts left: ' + (3 - count) + '\n' + 'Total prize: ' + prize + '$' + '\n' + 'Possible prize on current attempt: ' + (count < 1 ? max_prize : (count < 2) ? normal_prize : min_prize) + '$' + '\n' + 'Enter guessed number: ');
         count++;
         if (guess == number && count == 1) {
             prize += max_prize;
-            console.log('Its correct! Your prize is: ' + prize);
+            console.log('Its correct! Your prize is: ' + prize + '$');
             let t1 = confirm('Do you want to continue?');
             if (t1) {
                 step = step * 2;
@@ -26,13 +26,13 @@ if (start) {
                 continue;
             }
             else {
-                console.log("Final prize is: " + prize);
+                console.log("Final prize is: " + prize + '$');
                 break;
             }
         }
         else if (guess == number && count == 2) {
             prize += normal_prize;
-            console.log('Its correct! Your prize is: ' + prize);
+            console.log('Its correct! Your prize is: ' + prize + '$');
             let t1 = confirm('Do you want to continue?');
             if (t1) {
                 step = step * 2;
@@ -44,13 +44,13 @@ if (start) {
                 continue;
             }
             else {
-                console.log("Final prize is: " + prize);
+                console.log("Final prize is: " + prize + '$');
                 break;
             }
         }
         else if (guess == number && count == 3) {
             prize += min_prize;
-            console.log('Its correct! Your prize is: ' + prize);
+            console.log('Its correct! Your prize is: ' + prize + '$');
             let t1 = confirm('Do you want to continue?');
             if (t1) {
                 step = step * 2;
@@ -62,12 +62,13 @@ if (start) {
                 continue;
             }
             else {
-                console.log("Final prize is: " + prize);
+                console.log("Final prize is: " + prize + '$');
                 break;
             }
         }
         else if (guess != number && count == 3) {
-            let t1 = confirm('Thanks for playing! Your prize is: ' + prize + ' Do you want to play again?');
+            console.log('Thanks for playing! Your prize is: ' + prize + '$');
+            let t1 = confirm(' Do you want to play again?');
             if (t1) {
                 count = 0;
                 prize = 0;
@@ -79,7 +80,7 @@ if (start) {
                 continue;
             }
             else {
-                console.log("Final prize is: " + prize);
+                console.log("Final prize is: " + prize + '$');
                 break;
             }
         }
