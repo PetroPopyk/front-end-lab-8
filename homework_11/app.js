41e4c5144f6a5b834f0c3fd71d80c9ead125de4f
Petro Popyk <happyfairy228@gmail.com>
2018-02-23 15:15:27 +0200

    new commit

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,7 +1,7 @@
 var acc = document.getElementsByClassName("accordion");
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" clas,
+        /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
         /* Toggle between hiding and showing the active panel */
